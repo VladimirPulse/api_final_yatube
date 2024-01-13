c9fe80b77cb0cc52ff107331c64da01714e691b4
Владимир Кулаков <VrachKulakovVS@mail.ru>
2024-01-13 10:16:31 +0300

    step 1 app.py changes

--- yatube_api/api/apps.py
+++ yatube_api/api/apps.py
@@ -5,5 +5,4 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     """Класс регистрации приложения api."""
 
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
